71a82074ff18e5ab2185fd0753005a97d31d1e63
Summer Rizzo <srainrizzo@gmail.com>
2019-12-10 23:37:24 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    def initialize (title)
+        @title = title
+    end
 
+    #Getter for book title
+    attr_reader :title
+    #Getter and Setter for book author, genre, and page count
+    attr_accessor :author, :genre, :page_count
+
+    #Method that 'turns the page' of the book
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+    def initialize (brand)
+        @brand = brand
+    end
+
+    #Getter for shoe brand
+    attr_reader :brand
+    #Getter and Setter for shoe color, size, material, and condition
+    attr_accessor :color, :size, :material, :condition
+
+    #Method that 'turns the page' of the book
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
